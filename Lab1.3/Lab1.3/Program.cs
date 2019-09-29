8a431fcfde9d8b3f0cb4862066b9230b8191a1b0
ilia-vlasov <54813599+ilia-vlasov@users.noreply.github.com>
2019-09-29 16:14:50 +0300

    Add files via upload

---
+++ Lab1.3/Lab1.3/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab1._3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double a, b, sr;
+            Console.Write("Введите а: ");
+            a = int.Parse(Console.ReadLine());
+            Console.Write("Введите b: ");
+            b = int.Parse(Console.ReadLine());
+            sr = (a + b) / 2;
+            Console.WriteLine(string.Format("Ср. Ариф: {0}", sr));
+            Console.ReadKey();
+        }
+    }
+}
