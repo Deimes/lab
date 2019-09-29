961031fa565d8982d1b20008098849f32d171348
ilia-vlasov <54813599+ilia-vlasov@users.noreply.github.com>
2019-09-29 16:26:25 +0300

    Add files via upload

---
+++ Lab2.4/Lab2.4/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab2._4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double h, shir, x1, y1, x2, y2, p, s;
+            Console.Write("Введите координату x1: ");
+            x1 = int.Parse(Console.ReadLine());
+
+            Console.Write("Введите координату y1: ");
+            y1 = int.Parse(Console.ReadLine());
+
+            Console.Write("Введите координату x2: ");
+            x2 = int.Parse(Console.ReadLine());
+
+            Console.Write("Введите координату y2: ");
+            y2 = int.Parse(Console.ReadLine());
+
+            h = Math.Abs(y2 - y1);
+            shir = Math.Abs(x2 - x1);
+
+            p = 2 * (h + shir);
+            s = h * shir;
+
+            Console.WriteLine(string.Format("Периметр данного прямоугольника равен: {0}, Площадь равна: {1}", p, s));
+            Console.ReadKey();
+
+
+
+        }
+    }
+}
---
+++ Lab2.5/Lab2.5/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab2._5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double x1, y1, x2, y2, x3, y3, p, s, a1, a2, a3;
+
+            Console.Write("Введите координату x1: ");
+            x1 = int.Parse(Console.ReadLine());
+
+            Console.Write("Введите координату y1: ");
+            y1 = int.Parse(Console.ReadLine());
+
+            Console.Write("Введите координату x2: ");
+            x2 = int.Parse(Console.ReadLine());
+
+            Console.Write("Введите координату y2: ");
+            y2 = int.Parse(Console.ReadLine());
+
+            Console.Write("Введите координату x3: ");
+            x3 = int.Parse(Console.ReadLine());
+
+            Console.Write("Введите координату y3: ");
+            y3 = int.Parse(Console.ReadLine());
+
+            a1 = Math.Abs(Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2)));
+            a2 = Math.Abs(Math.Sqrt(Math.Pow(x3 - x2, 2) + Math.Pow(y3 - y2, 2)));
+            a3 = Math.Abs(Math.Sqrt(Math.Pow(x1 - x3, 2) + Math.Pow(y1 - y3, 2)));
+
+            p = (a1 + a2 + a3);
+            s = Math.Sqrt(p * (p - a1) * (p - a2) * (p - a3)) / 2;
+
+            Console.WriteLine(string.Format("Периметр равен: {0:N2}, Площадь равна: {1:N2}", p, s));
+            Console.ReadKey();
+
+
+        }
+    }
+}
