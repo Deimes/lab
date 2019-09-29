81cb4dc9352a1dfb79db98fe75e78474d2b4e9f7
ilia-vlasov <54813599+ilia-vlasov@users.noreply.github.com>
2019-09-29 16:23:08 +0300

    Add files via upload

---
+++ Lab1.1/Lab1.1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab1._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b, s, p;
+            Console.Write("Введите а: ");
+            a = int.Parse(Console.ReadLine());
+            Console.Write("Введите b: ");
+            b = int.Parse(Console.ReadLine());
+            s = a * b;
+            p = 2 * (a + b);
+            Console.WriteLine(string.Format("Периметр равен: {0} ", p));
+            Console.WriteLine(string.Format("Площадь равна: {0}", s));
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Lab1.4/Lab1.4/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab1._4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double a, b, summ, razn, proiz, chast;
+            Console.Write("Введите а: ");
+            a = int.Parse(Console.ReadLine());
+            Console.Write("Введите b: ");
+            b = int.Parse(Console.ReadLine());
+
+            a = Math.Pow(a, 2);
+            b = Math.Pow(b, 2);
+
+            summ = a + b;
+            razn = a - b;
+            proiz = a * b;
+            chast = a / b;
+
+            if (a != 0 && b != 0)
+            {
+                Console.WriteLine(string.Format("Сумма равна: {0}", summ));
+                Console.WriteLine(string.Format("Разность равна: {0}", razn));
+                Console.WriteLine(string.Format("Произведение равно: {0}", proiz));
+                Console.WriteLine(string.Format("Частное равно: {0}", chast));
+            }
+            else
+            {
+                Console.WriteLine("Вы ввели недопустимое значение!");
+            }
+            Console.ReadKey();
+        }
+    }
+}
