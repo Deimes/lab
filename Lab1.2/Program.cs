a82c3f0c0889d4aab3a7ca328c345b4048337cc1
Илья Власов <Klarkuk23@gmail.com>
2019-09-29 16:01:35 +0300

    Добавьте файлы проекта.

---
+++ Lab1.2/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab1._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int d;
+            double pi, l;
+            pi = 3.14;
+            Console.Write("Введите диаметр: ");
+            d = int.Parse(Console.ReadLine());
+            l = pi * d;
+            Console.WriteLine(string.Format("Длина равна: {0}", l));
+            Console.ReadKey();
+        }
+    }
+}
