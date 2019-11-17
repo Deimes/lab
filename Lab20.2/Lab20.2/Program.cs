f7e2a58ccf8898a09bcc60c5eae69c6699cd6b4a
ilia-vlasov <54813599+ilia-vlasov@users.noreply.github.com>
2019-11-17 18:04:10 +0300

    Add files via upload

---
+++ Lab20.2/Lab20.2/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace Lab20._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s;
+            Write("Введите строку: ");
+            s = ReadLine();
+            for (int i = 0; i < s.Length; i++)
+            {
+                Write(s[i] + " ");
+            }
+            ReadKey();
+
+
+        }
+    }
+}
