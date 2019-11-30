2c8c983c1b45bc45e44a543f1929d237fcfb7112
ilia-vlasov <54813599+ilia-vlasov@users.noreply.github.com>
2019-12-01 01:08:42 +0300

    Add files via upload

---
+++ Lab21.7/Lab21.7/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace Lab21._7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s;
+            Write("Введите строку: ");
+            s = ReadLine();
+
+            for (int i = 0; i < s.Length; i++)
+            {
+                if (i % 2 != 0)
+                {
+                    Write(s[i]);
+                }
+            }
+            for (int i = s.Length - 1; i >= 0; i--)
+            {
+                if (i % 2 == 0)
+                {
+                    Write(s[i]);
+                }
+            }
+            
+            ReadKey();
+
+
+        }
+    }
+}
