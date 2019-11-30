5f4a8553247cf074a5c95fe1d5f1db18f684a0e0
ilia-vlasov <54813599+ilia-vlasov@users.noreply.github.com>
2019-12-01 01:07:20 +0300

    Add files via upload

---
+++ Lab21.5/Lab21.5/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+using System.IO;
+
+namespace Lab21._5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string FullPath;
+            Write("Введите полный путь к файлу: ");
+            FullPath = ReadLine();      
+            Write(Path.GetFileNameWithoutExtension(FullPath));         
+            ReadKey();
+
+        }
+    }
+}
